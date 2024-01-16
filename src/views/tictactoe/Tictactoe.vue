76ca0b3a1ba96e9657a02a5b77488c29b5f9f934
co2color <819109255@qq.com>
2024-01-16 23:57:00 +0800

    feat

--- src/views/tictactoe/Tictactoe.vue
+++ src/views/tictactoe/Tictactoe.vue
@@ -1,20 +1,54 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { ref, nextTick } from 'vue'
 
-const board = ref(new Array(9).fill(''))
+function getInitBoard() {
+  return new Array(9).fill('')
+}
+
+const board = ref(getInitBoard())
 
 // 定义当前玩家
 const currentPlayer = ref('X')
 
 function makeMove(index: number) {
   board.value[index] = currentPlayer.value
+
+  if (checkWin()) {
+    console.log(`${currentPlayer.value} win!`)
+    return
+  }
   currentPlayer.value = currentPlayer.value === 'X' ? 'O' : 'X'
 }
-function resetBoard() {}
+function resetBoard() {
+  board.value = getInitBoard()
+  currentPlayer.value = 'X'
+}
+
+function checkWin() {
+  const winArr = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8], // 横
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8], // 竖
+    [0, 4, 8],
+    [2, 4, 6], // 斜
+  ]
+  return winArr.some((item) => {
+    const [a, b, c] = item
+    return (
+      board.value[a] &&
+      board.value[a] === board.value[b] &&
+      board.value[a] === board.value[c]
+    )
+  })
+}
 </script>
 
 <template>
   <div>
+    {{ board }}
     <h1>Tic Tac Toe</h1>
     <div class="board">
       <div v-for="(cell, index) in board" :key="index" @click="makeMove(index)">
