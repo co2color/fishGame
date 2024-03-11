5d12236e246b1828d7eb27643d935f8fcddccf82
co2color <819109255@qq.com>
2024-03-11 23:11:31 +0800

    feat

--- src/views/sokoban/sokoban.vue
+++ src/views/sokoban/sokoban.vue
@@ -36,11 +36,12 @@ function initMap() {
   mapList.value[bx][by].type = EmapType.Ball
 }
 
-// 上就是y--，下就是y++，左就是x--，右就是x++
+// 往上，x不动，y--
 function moveUp() {
   const { x, y } = player.value
   const currentType = mapList.value[x][y].type
   const topType = mapList.value[x][y - 1].type
+  // 能往上的条件有两个：1是上面是空的，2是上面有球且球的上面是空的
   if (topType === EmapType.Empty) {
     player.value = { x, y: y - 1 }
     mapList.value[x][y].type = EmapType.Empty
@@ -49,10 +50,40 @@ function moveUp() {
 
   // 如果上面是球，就要判断球的上面是不是空的
   else if (topType === EmapType.Ball) {
-    const ballTop = mapList.value[x][y - 2].type
+    const ballTop = mapList.value[x][y - 2].type // 球的上面
+    // 如果球的上面是空的，就可以推，将玩家和球都往上移动
+    if (ballTop === EmapType.Empty) {
+      player.value = { x, y: y - 1 } // 玩家往上走
+      ball.value = { x, y: y - 2 } // 球往上走
+      mapList.value[x][y].type = EmapType.Empty // 玩家原来的位置变成空
+      mapList.value[x][y - 1].type = currentType // 球原来的位置变成玩家
+      mapList.value[x][y - 2].type = EmapType.Ball // 球的上面变成球
+    }
   }
 }
-function moveDown() { }
+
+// 往下，x不动，y++
+function moveDown() {
+  const { x, y } = player.value
+  const currentType = mapList.value[x][y].type
+  // 下面是空的，就往下走
+  const bottomType = mapList.value[x][y + 1].type
+  if (bottomType === EmapType.Empty) {
+    player.value = { x, y: y + 1 }
+    mapList.value[x][y].type = EmapType.Empty
+    mapList.value[x][y + 1].type = currentType
+  }
+  else if (bottomType === EmapType.Ball) {
+    const ballBottom = mapList.value[x][y + 2].type
+    if (ballBottom === EmapType.Empty) {
+      player.value = { x, y: y + 1 }
+      ball.value = { x, y: y + 2 }
+      mapList.value[x][y].type = EmapType.Empty
+      mapList.value[x][y + 1].type = currentType
+      mapList.value[x][y + 2].type = EmapType.Ball
+    }
+  }
+}
 function moveLeft() { }
 function moveRight() { }
 
@@ -131,7 +162,7 @@ onMounted(() => {
   justify-content: center;
   font-size: 12px;
   font-weight: bold;
-  transition: all 0.3s;
+  transition: all 2s;
   transform: scale(0.8);
 }
 
