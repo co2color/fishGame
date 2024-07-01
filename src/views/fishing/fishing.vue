867fa2dd5a06e928bb0e26e7fdf7ed619b925304
co2color <819109255@qq.com>
2024-07-01 23:49:32 +0800

    feat

--- src/views/fishing/fishing.vue
+++ src/views/fishing/fishing.vue
@@ -1,28 +1,28 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 // 这项目想模仿原神的钓鱼，但好像有点麻烦。
 // 还没做完，有空再说。。。
 const is_click_btn = ref(false)
-const btnMouseDownClick = (e: MouseEvent) => {
+function btnMouseDownClick(e: MouseEvent) {
   is_click_btn.value = true
   console.log(e)
 }
-const btnMouseUpClick = (e: MouseEvent) => {
+function btnMouseUpClick(e: MouseEvent) {
   if (is_click_btn.value) {
   }
   is_click_btn.value = false
 }
-const btnClick = (e: MouseEvent) => {
+function btnClick(e: MouseEvent) {
   // console.log(e)
   p.is_start.value ? p.stop() : p.start()
 }
 onMounted(() => {
   document.addEventListener('mouseup', btnMouseUpClick)
 })
-const useProgressMove = (step: number = 1) => {
+function useProgressMove(step: number = 1) {
   const progress_left_px = ref('0px')
   let position = 'right'
-  let is_start = ref(false)
+  const is_start = ref(false)
   let t = 0
   const start = () => {
     is_start.value = true
@@ -36,12 +36,12 @@ const useProgressMove = (step: number = 1) => {
         position = 'right'
         temp_num += step
       }
-      if (position === 'right') {
-        progress_left_px.value = temp_num + step + 'px'
-      } else {
-        progress_left_px.value = temp_num - step + 'px'
-      }
-      progress_left_px.value = (position === 'right' ? temp_num + step : temp_num - step) + 'px'
+      if (position === 'right')
+        progress_left_px.value = `${temp_num + step}px`
+      else
+        progress_left_px.value = `${temp_num - step}px`
+
+      progress_left_px.value = `${position === 'right' ? temp_num + step : temp_num - step}px`
     }, 10)
   }
   const stop = () => {
@@ -58,9 +58,9 @@ const { progress_left_px, is_start } = p
 <template>
   <div class="flex flex-col justify-center">
     <div class="box">
-      <div class="box-progress"></div>
+      <div class="box-progress" />
     </div>
-    <button @mousedown="btnMouseDownClick" @click="btnClick" id="btn" class="mt-10">
+    <button id="btn" class="mt-10" @mousedown="btnMouseDownClick" @click="btnClick">
       {{ is_start ? '暂停' : '启动' }}
     </button>
   </div>
